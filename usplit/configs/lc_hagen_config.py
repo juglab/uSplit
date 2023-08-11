18c4cc87aaabbb47d324b8bbf076a969c136e6ae
Ashesh <ashesh276@gmail.com>
2023-08-11 10:25:39 +0200

    config update

--- usplit/configs/lc_hagen_config.py
+++ usplit/configs/lc_hagen_config.py
@@ -44,7 +44,7 @@ def get_config():
     loss.kl_annealtime = 10
     loss.kl_start = -1
     loss.kl_min = 1e-7
-    loss.free_bits = 1.0
+    loss.free_bits = 0.0
 
     model = config.model
     model.model_type = ModelType.LadderVae
