ac257fd4aa4d3fe2f00707c628af49fcb36a75e5
Ashesh <ashesh276@gmail.com>
2025-02-27 00:17:30 +0100

    updated script

--- examples/htt24_realinput.sh
+++ examples/htt24_realinput.sh
@@ -3,3 +3,16 @@ python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disen
 python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D12-M3-S0-L0/4 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_T24/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
 
 python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D12-M10-S0-L3/1 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_T24/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
+
+
+
+# HT-LIF24
+DeepLC: [gnode12/2502/D11-M3-S0-L0/12](https://wandb.ai/juglab/Disentanglement/runs/jqbipyne)
+RegularLC: [gnode13/2502/D11-M3-S0-L0/11](https://wandb.ai/juglab/Disentanglement/runs/535ajc4x)
+LeanLC: [lin-jug-m-03/2502/D11-M3-S0-L0/9](https://wandb.ai/juglab/Disentanglement/runs/ynjwnwye)
+UNet: [gnode04/2502/D11-M10-S0-L3/2](https://wandb.ai/juglab/Disentanglement/runs/d8izac99)
+
+python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D11-M3-S0-L0/12 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
+python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D11-M3-S0-L0/11 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
+python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D11-M3-S0-L0/9 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
+python examples/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2502/D11-M10-S0-L3/2 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --notebook=/home/ashesh.ashesh/code/uSplit/examples/EvaluateRealInput.ipynb
